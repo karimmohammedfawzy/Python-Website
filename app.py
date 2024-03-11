1a17658ba4012ed9d4059da0731f74fdc9085382
karimmohammedfawzy <108751526+karimmohammedfawzy@users.noreply.github.com>
2024-03-11 12:02:15 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+#----Creating a Website----
+
+import streamlit as st
+
+
+#create the name in the tab menu
+st.set_page_config(page_title="Contact Us", page_icon=":Happy:", layout="wide")
+
+
+
+#-------Header Section-------
+
+st.subheader("Hi, My name is Karim ")
+st.title("Karim")
+st.write("Shebin Elkom")
+
+
+
+
+#----What i do Section---- 
+
+
+with st.container():
+    st.write('---')
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What i Do in my life")
+        st.write("##")
+        st.write("Hello")
+        st.write("[instagram account >](https://www.instagram.com/karimmohamed04/)")
